46c7234cebaf29af2f9a58f3bd644167ee53992f
Orsolya Torokne Bus <orsolyabus@gmail.com>
2019-01-25 13:47:25 -0800

    style favorites tab

--- src/components/Account/Favorites.js
+++ src/components/Account/Favorites.js
@@ -1,10 +1,22 @@
 import React from 'react';
-import {  } from "mdbreact";
+import { MDBCardGroup } from "mdbreact";
+import FavoriteCard from './FavoriteCard';
 
 const Favorites = () => (
   <div className="Favorites" >
     <h3>Favorites</h3>
+    <MDBCardGroup column>
+      <FavoriteCard />
+      <FavoriteCard />
+      <FavoriteCard />
+      <FavoriteCard />
+      <FavoriteCard />
+      <FavoriteCard />
+      <FavoriteCard />
+      <FavoriteCard />
+      <FavoriteCard />
+    </MDBCardGroup>
   </div>
 );
-
+// szinekkel vagy massal megkulonboztetni a postot orat tanart helyszint
 export default Favorites;
