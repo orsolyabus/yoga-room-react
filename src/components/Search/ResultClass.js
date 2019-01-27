9d2b157fba2283257cd87e56fbb7e34602f06561
Orsolya Torokne Bus <orsolyabus@gmail.com>
2019-01-26 22:04:11 -0800

    format search results

---
+++ src/components/Search/ResultClass.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { MDBCol, MDBRow } from "mdbreact";
+
+const ResultClass = () => (
+  <div className="ResultClass">
+    <h4>Title</h4>
+    <MDBRow>
+      <MDBCol>        
+        <h6>location</h6>
+        <h6>Teacher</h6>
+        <h6>time</h6>
+      </MDBCol>
+      <MDBCol>
+        <h6>book online spots: 5/12</h6>
+        <h6></h6>
+      </MDBCol>
+    </MDBRow>
+    
+  </div>
+);
+
+export default ResultClass;
--- src/components/Search/ResultList.js
+++ src/components/Search/ResultList.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import { MDBCard, MDBListGroup, MDBListGroupItem } from "mdbreact";
+import ResultClass from './ResultClass';
 
 const ResultList = () => (
   <div className="ResultList">
    <MDBCard >
     <MDBListGroup>
-      <MDBListGroupItem>result class 1</MDBListGroupItem>
-      <MDBListGroupItem>result class 1</MDBListGroupItem>
-      <MDBListGroupItem>result class 1</MDBListGroupItem>
+      <MDBListGroupItem>
+        <ResultClass />
+      </MDBListGroupItem>
+      <MDBListGroupItem>
+        <ResultClass />
+      </MDBListGroupItem>
+      <MDBListGroupItem>
+        <ResultClass />
+      </MDBListGroupItem>
     </MDBListGroup>
   </MDBCard>
 
