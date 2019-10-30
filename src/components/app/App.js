8c6419503802f74c4d49a33b7519d02c7ee3098a
Orsolya Torokne Bus <orsolyabus@gmail.com>
2019-10-30 12:15:20 -0700

    add footer

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import LandingPage from '../Landing/LandingPage';
+import LandingPage from '../Landing/LandingPage.jsx';
 import SearchPage from '../Search/SearchPage';
 import AccountPage from '../Account/AccountPage';
 // import 'font-awesome/css/font-awesome.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.scss';
 import NavBar from './NavBar';
+import Footer from './Footer';
 import { BrowserRouter, Route } from "react-router-dom";
 
 const App = () => (
@@ -15,8 +16,10 @@ const App = () => (
       <Route path="/" exact component={LandingPage} />
       <Route path="/search" exact component={SearchPage} />
       <Route path="/account" exact component={AccountPage} />
+      <Footer />
     </div>
   </BrowserRouter>
+  
 );
 
 
