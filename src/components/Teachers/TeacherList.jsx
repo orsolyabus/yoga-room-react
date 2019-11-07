216fe86db8f11c89124d7c5ad96fe0d382473f07
Orsolya Torokne Bus <orsolyabus@gmail.com>
2019-11-07 14:09:50 -0800

    implement server connection to get basic teachers data

--- src/components/Teachers/TeacherList.jsx
+++ src/components/Teachers/TeacherList.jsx
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import TeacherCard from './TeacherCard'
 import CardColumns from 'react-bootstrap/CardColumns';
+import { Teacher } from "../requests";
+import Spinner from 'react-bootstrap/Spinner'
 
 const TeachersList = () => {
-  const teachers = [1,2,3,4,2,2];
-  console.log(teachers);
-  
+  const [teachers, setTeachers] = useState({ teachers: [] });
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    Teacher.all().then(Ts => {
+      setTeachers(Ts)
+      setLoading(false)
+    })
+  }, []);
+  if (loading) return (
+    <Spinner animation="border" role="status" variant="primary">
+      <span className="sr-only">Loading...</span>
+    </Spinner>
+  )
+
   return (
-  <CardColumns>
-    {teachers.map(() => <TeacherCard/>)}
-  </CardColumns>
-)};
+    <CardColumns>
+      {teachers.map((t) => <TeacherCard key={t.id} teacher={t} />)}
+    </CardColumns>
+  )
+};
 
 export default TeachersList;
